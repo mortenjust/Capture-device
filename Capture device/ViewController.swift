c9e760b16c3f5fe9bb2a1bf3ec0ac7669edb9634
Morten Just <morten.just@genstart.dk>
2021-05-15 09:19:45 +0200

    Initial Commit

---
+++ Capture device/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Capture device
+//
+//  Created by Morten Just on 5/15/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
